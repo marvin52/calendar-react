19ffe5200a6c48b2958f38b650b38258e1f890ba
Marvin Medeiros <marvin.medeiros52@gmail.com>
2016-11-29 03:23:24 -0200

    Move .js to .jsx, over the airbnb eslint pattern

--- src/js/client.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Calendar from "./Calendar"
-
-const app = document.getElementById('app');
-
-require('./../css/styles.sass')
-
-ReactDOM.render(<Calendar/>, app);
--- src/js/components/Sub.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default class Sub extends React.Component {
-	render(){
-		return (
-			<p className="calendar__holiday-sub">{this.props.hl.date.split('-')[2]} - {this.props.hl.name}</p>
-		)
-	}
-}
